370bbce61a01bcca9cd1ad06f361e7a7f43b69eb
Kamer <68508755+K4m33r@users.noreply.github.com>
2024-06-02 21:05:55 +0300

    Day 1

---
+++ bandname.py
@@ -0,0 +1,6 @@
+print("Welcome to band name generator game.!!")
+name1 = input("What is your city name: ")
+print('\n')
+name2 = input("What is your pet name: ")
+print('\n')
+print("Your band name is: " + name1 +" "+ name2)
